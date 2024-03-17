e5635915444cd0517302f65ec9a17bde36fb7423
Juan Antonio Cortes Elrio <152733103+Mogurkazan@users.noreply.github.com>
2024-03-17 17:51:04 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,42 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { arrow } from "@popperjs/core";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //Generate the const of cards
+  const cardNumber = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  //Generate the const of suits
+  const deckSuits = ["♦", "♥", "♠", "♣"];
+  //Change color of diamons and hearths
+  deckSuits[0] = '<span style="color:red;">' + deckSuits[0] + "</span>"; // Diamonds
+  deckSuits[1] = '<span style="color:red;">' + deckSuits[1] + "</span>"; // Hearths
+  //Generate random number with array length
+  function generateRandomNumber(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
 };
